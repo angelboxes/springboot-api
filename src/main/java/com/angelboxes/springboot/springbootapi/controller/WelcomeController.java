0a9fbfab3d15358ebb145419ea0e568fa1c1180d
Angel Cajas <angel.cj.o@gmail.com>
2020-07-05 01:16:01 -0600

    Up until Step 17

--- src/main/java/com/angelboxes/springboot/springbootapi/controller/WelcomeController.java
+++ src/main/java/com/angelboxes/springboot/springbootapi/controller/WelcomeController.java
@@ -1,9 +1,13 @@
 package com.angelboxes.springboot.springbootapi.controller;
 
+import com.angelboxes.springboot.springbootapi.configuration.BasicConfiguration;
 import com.angelboxes.springboot.springbootapi.service.WelcomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class WelcomeController {
@@ -11,9 +15,21 @@ public class WelcomeController {
     @Autowired
     WelcomeService welcomeService;
 
+    @Autowired
+    private BasicConfiguration configuration;
+
     @GetMapping("/welcome")
-    public String welcome(){
+    public String welcome() {
         return welcomeService.retrieveWelcomeMessage();
+    }
+
+    @GetMapping("/dynamic-configuration")
+    public Map dynamicConfiguration() {
+        Map map = new HashMap<>();
+        map.put("message", configuration.getMessage());
+        map.put("number", configuration.getNumber());
+        map.put("value", configuration.isValue());
+        return map;
     }
 
 }
